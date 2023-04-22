d89e7c37d7984ebe4faec2478420ae45755fc107
GraysonSuchanek <graysuch@gmail.com>
2023-04-22 16:29:15 -0700

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header() {
+  return (
+    <div class='container'>
+      <div class="row">
+        <div class="col">DATE</div>
+        <div class="col-6">TITLE</div>
+        <div class="col">MENU</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
