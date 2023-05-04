272780e5159727925c46acbf2574b7c12e040373
GraysonSuchanek <graysuch@gmail.com>
2023-05-04 14:13:00 -0700

    header and title start

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/Header.css";
 
-function Header() {
+function Header({ title, menu }) {
+  let currentDay = new Date().toLocaleDateString("en-US", { day: "2-digit" });
+  let currentMonth = new Date().toLocaleDateString("en-US", { month: "short" });
+
   return (
-    <div class='container'>
+    <div class="container headerBoarder">
       <div class="row">
-        <div class="col">DATE</div>
-        <div class="col-6">TITLE</div>
-        <div class="col">MENU</div>
+        <div class="col headerOption date">
+          <div class="row">{currentDay}</div>
+          <div class="row">{currentMonth}</div>
+        </div>
+        <div class="col-6 headerOption centerOption">{title}</div>
+        <div class="col headerOption">MENU</div>
       </div>
     </div>
   );
---
+++ src/components/Tasks.js
@@ -0,0 +1,5 @@
+function Tasks() {
+  return <div>Hello</div>;
+}
+
+export default Tasks;
